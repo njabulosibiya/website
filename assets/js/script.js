1943cbf63e2e1fd7d8757e80092ac1a8157bbaa1
Njabulo Sibiya <75039043+njabulosibiya@users.noreply.github.com>
2022-12-08 16:04:44 +0200

    Initial commit of my website NB: multiple changes made before

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+// Parallax Code
+        var scene = document.getElementById('scene');
+        var parallax = new Parallax(scene);//changing 'p' to 'P' changes 404.html frm smooth animation to not smooth but with parallax effects.
